6b344ca5a20e3a2ceae913e1b582d9665a31b5db
unknown <cristian.lopez-a@labinfo.is.escuelaing.edu.co>
2020-01-17 12:57:57 -0500

    inicial

---
+++ tarea-uno/src/main/java/edu/escuelaing/arem/ASE/app/clases/LinkedList.java
@@ -0,0 +1,115 @@
+package edu.escuelaing.arem.ASE.app.clases;
+
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
+
+class LinkedList<E> implements List<E> {
+    private int size=0;
+    private Node<E> head = new Node<E>();
+    private Node<E> actual = null;
+
+    public int size() {
+        return size;
+    }
+
+    public boolean isEmpty() {
+        return false;
+    }
+
+    public boolean contains(Object o) {
+        return false;
+    }
+
+    public Iterator<E> iterator() {
+        return new ListIterator<E>(head.getPrior());
+    }
+
+    public Object[] toArray() {
+        return null;
+    }
+
+    public <T> T[] toArray(T[] a) {
+        return null;
+    }
+
+    public boolean add(E e) {
+        Node<E> nuevo = new Node<E>(e);
+        if (size==0){
+            head.setPrior(nuevo);
+            head.setNext(nuevo);
+            this.actual = nuevo;
+        }else{
+            head.getNext().setNext(nuevo);
+            head.setNext(nuevo);
+        }
+        size++;
+        return true;
+    }
+
+    public boolean remove(Object o) {
+        return false;
+    }
+
+    public boolean containsAll(Collection<?> c) {
+        return false;
+    }
+
+    public boolean addAll(Collection<? extends E> c) {
+        return false;
+    }
+
+    public boolean addAll(int index, Collection<? extends E> c) {
+        return false;
+    }
+
+    public boolean removeAll(Collection<?> c) {
+        return false;
+    }
+
+    public boolean retainAll(Collection<?> c) {
+        return false;
+    }
+
+    public void clear() {
+
+    }
+
+    public E get(int index) {
+        return null;
+    }
+
+    public E set(int index, E element) {
+        return null;
+    }
+
+    public void add(int index, E element) {
+
+    }
+
+    public E remove(int index) {
+        return null;
+    }
+
+    public int indexOf(Object o) {
+        return 0;
+    }
+
+    public int lastIndexOf(Object o) {
+        return 0;
+    }
+
+    public ListIterator<E> listIterator() {
+        return null;
+    }
+
+    public ListIterator<E> listIterator(int index) {
+        return null;
+    }
+
+    public List<E> subList(int fromIndex, int toIndex) {
+        return null;
+    }
+    
+}
---
+++ tarea-uno/src/main/java/edu/escuelaing/arem/ASE/app/clases/ListIterator.java
@@ -0,0 +1,16 @@
+package edu.escuelaing.arem.ASE.app.clases;
+
+import java.util.Iterator;
+
+class ListIterator<E> implements Iterator<E> {
+    Node<E> current;
+
+    public boolean hasNext() {
+        return false;
+    }
+
+    public E next() {
+        return null;
+    }
+    
+}
