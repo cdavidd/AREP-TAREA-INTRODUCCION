39ea12fad36a14ba77c86f0a40ab881a5b89b8be
david <cdavidlopez@hotmail.com>
2020-01-23 22:36:26 -0500

    documentacion

--- trabajo/src/main/java/edu/escuelaing/arem/ASE/app/model/ListIteratorPersonal.java
+++ trabajo/src/main/java/edu/escuelaing/arem/ASE/app/model/ListIteratorPersonal.java
@@ -5,14 +5,29 @@ import java.util.Iterator;
 class ListIteratorPersonal<E> implements Iterator<E> {
     Node<E> current;
 
+    /**
+     * Crea el iterador personal
+     * 
+     * @param actual
+     */
     public ListIteratorPersonal(Node<E> actual) {
         current = actual;
     }
 
+    /**
+     * Compara si tiene un siguiente el nodo actual
+     * 
+     * @return boolean
+     */
     public boolean hasNext() {
         return current != null;
     }
 
+    /**
+     * Retorna el valor del nodo actual
+     * 
+     * @return E
+     */
     public E next() {
         E data = current.getData();
         current = current.getNext();
--- trabajo/src/main/java/edu/escuelaing/arem/ASE/app/model/Node.java
+++ trabajo/src/main/java/edu/escuelaing/arem/ASE/app/model/Node.java
@@ -8,30 +8,62 @@ class Node<E> {
     public Node() {
     }
 
+    /**
+     * Contruye un Nodo
+     */
     public Node(E dato) {
         this.data = dato;
     }
 
+    /**
+     * 
+     * @return retorna la informacion guardada en el nodo
+     */
     public E getData() {
         return data;
     }
 
+    /**
+     * Cambia el balor guardado en el nodo
+     * 
+     * @param data
+     */
     public void setData(E data) {
         this.data = data;
     }
 
+    /**
+     * Retorna el siguiente nodo apuntado
+     * 
+     * @return Nodo
+     */
     public Node<E> getNext() {
         return next;
     }
 
+    /**
+     * Cambia el el Nodo al que apunta
+     * 
+     * @param next
+     */
     public void setNext(Node<E> next) {
         this.next = next;
     }
 
+    /**
+     * Obtiene el nodo anterior
+     * 
+     * @return
+     */
     public Node<E> getPrior() {
         return prior;
     }
 
+    /**
+     * Cambia el nodo apuntado anterior
+     * 
+     * @param prior
+     */
     public void setPrior(Node<E> prior) {
         this.prior = prior;
     }
